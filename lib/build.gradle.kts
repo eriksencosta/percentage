6ad5f76dad9978fdddc950ae8d4579124b23cd29
Eriksen Costa <eriksencosta@gmail.com>
2024-03-03 20:11:34 +0000

    Rewrite Gradle setup

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -1,10 +1,7 @@
 import cl.franciscosolis.sonatypecentralupload.SonatypeCentralUploadTask
 import java.util.Locale
 
-// TODO: this is reserved for automating snapshot builds.
-// This will require changes on sonatype-central-upload.
-
-// Create a snapshot release by default.
+// TODO: this is reserved for automating snapshot builds. Create a snapshot release by default.
 val snapshotOption = System.getenv("SNAPSHOT_BUILD").let {
     it.isNullOrBlank() || "yes" == it.lowercase(Locale.getDefault())
 }
@@ -19,12 +16,9 @@ version = System.getenv("BUILD_VERSION").let {
 }
 
 plugins {
-    // Apply the org.jetbrains.kotlin.jvm Plugin to add support for Kotlin.
-    alias(libs.plugins.jvm)
     `java-library`
     `maven-publish`
-
-    id("cl.franciscosolis.sonatype-central-upload") version "1.0.3"
+    alias(libs.plugins.sonatype.central.upload)
 }
 
 repositories {
@@ -32,9 +26,10 @@ repositories {
 }
 
 dependencies {
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    compileOnly(libs.kotlin.stdlib)
+    testImplementation(libs.kotlin.test.junit5)
     testImplementation(libs.junit.jupiter.engine)
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testRuntimeOnly(libs.junit.platform.launcher)
 }
 
 java {
@@ -45,20 +40,66 @@ java {
     withSourcesJar()
 }
 
-tasks.named<Test>("test") {
-    useJUnitPlatform()
-}
+tasks {
+    named<Test>("test") {
+        useJUnitPlatform()
+    }
 
-tasks.jar {
-    base {
-        manifest {
-            attributes(mapOf(
-                "Implementation-Title" to "${project.group}.${project.name}",
-                "Implementation-Version" to project.version
-            ))
+    named<Jar>("jar") {
+        base {
+            manifest {
+                attributes(mapOf(
+                    "Implementation-Title" to "${project.group}.${project.name}",
+                    "Implementation-Version" to project.version
+                ))
+            }
+        }
+    }
+
+    named<SonatypeCentralUploadTask>("sonatypeCentralUpload") {
+        username = System.getenv("MAVEN_CENTRAL_USERNAME")
+        password = System.getenv("MAVEN_CENTRAL_PASSWORD")
+
+        archives = files(*jars())
+        pom = file("build/publications/pom/pom-default.xml")
+
+        signingKey = System.getenv("GPG_PRIVATE_KEY")
+        signingKeyPassphrase = System.getenv("GPG_PRIVATE_KEY_PASSWORD")
+        publicKey = System.getenv("GPG_PUBLIC_KEY")
+
+        publishingType = "MANUAL"
+    }
+
+    named<Task>("build") {
+        dependsOn("generatePomFileForPomPublication")
+    }
+
+    named<Task>("sonatypeCentralUpload") {
+        mustRunAfter("build")
+    }
+
+    register("release") {
+        description = "Build and publish the library to Maven Central."
+        group = "Release"
+
+        dependsOn("build", "sonatypeCentralUpload")
+        mustRunAfter("sonatypeCentralUpload")
+    }
+
+    register("version") {
+        description = "Prints the version stamp."
+        group = "Verification"
+
+        doLast {
+            val versionMessage = "The current version stamp is: $version."
+            val snapshotMessage = "The snapshot option is: %s.".format(if (snapshotOption) "on" else "off")
+
+            logger.lifecycle(versionMessage)
+            logger.lifecycle(snapshotMessage)
         }
     }
 }
+
 
 publishing {
     publications {
@@ -101,49 +142,6 @@ publishing {
             }
         }
     }
-}
-
-tasks.named("build") {
-    dependsOn("generatePomFileForPomPublication")
-}
-
-tasks.named("sonatypeCentralUpload") {
-    mustRunAfter("build")
-}
-
-tasks.register("release") {
-    description = "Build and publish the library to Maven Central."
-    group = "Release"
-
-    dependsOn("build", "sonatypeCentralUpload")
-    mustRunAfter("sonatypeCentralUpload")
-}
-
-tasks.register("version") {
-    description = "Prints the version stamp."
-    group = "Verification"
-
-    doLast {
-        val versionMessage = "The current version stamp is: $version."
-        val snapshotMessage = "The snapshot option is: %s.".format(if (snapshotOption) "on" else "off")
-
-        logger.lifecycle(versionMessage)
-        logger.lifecycle(snapshotMessage)
-    }
-}
-
-tasks.named<SonatypeCentralUploadTask>("sonatypeCentralUpload") {
-    username = System.getenv("MAVEN_CENTRAL_USERNAME")
-    password = System.getenv("MAVEN_CENTRAL_PASSWORD")
-
-    archives = files(*jars())
-    pom = file("build/publications/pom/pom-default.xml")
-
-    signingKey = System.getenv("GPG_PRIVATE_KEY")
-    signingKeyPassphrase = System.getenv("GPG_PRIVATE_KEY_PASSWORD")
-    publicKey = System.getenv("GPG_PUBLIC_KEY")
-
-    publishingType = "MANUAL"
 }
 
 fun jars() = arrayOf(jarName(), jarName("javadoc"), jarName("sources"))
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,18 +1,3 @@
-pluginManagement {
-    resolutionStrategy {
-        eachPlugin {
-            if ("cl.franciscosolis.sonatype-central-upload" == requested.id.toString()) {
-                useModule("com.github.eriksencosta:SonatypeCentralUpload:publishing-type-option-jitpack-SNAPSHOT")
-            }
-        }
-    }
-
-    repositories {
-        gradlePluginPortal()
-        maven("https://jitpack.io")
-    }
-}
-
 plugins {
     // Apply the foojay-resolver plugin to allow automatic download of JDKs
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.7.0"
